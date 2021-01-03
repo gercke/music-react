3d6ae8e254e37e8a36d1d12624913d16b15109fa
gercke <986219381@qq.com>
2021-01-03 22:33:51 +0800

    关于新碟上架部分完成

--- src/pages/discover/c-pages/recommend/c-component/new-album/index.js
+++ src/pages/discover/c-pages/recommend/c-component/new-album/index.js
@@ -1,9 +1,57 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect,shallowEqual, useRef } from 'react'
 
 import SJQThemeHeaderRcm from '@/components/theme-header-recommend'
+import SJQAlbumCover from '@/components/album-cover'
+import { Carousel } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
+import { getNewAlbumAction } from '../../store/actionCreators'
+import { AlbumWrapper } from './style'
 
 export default memo(function SJQNewAlbum() {
+    //redux hooks
+    const { newAlbums } = useSelector(state=>({
+        newAlbums: state.getIn(["recommend","newAlbums"])
+    }),shallowEqual);
+
+    const dispatch = useDispatch();
+    
+    const pageRef = useRef();
+    useEffect(()=>{
+        dispatch(getNewAlbumAction(10));
+    },[dispatch]);
+
     return (
-        <SJQThemeHeaderRcm title="新碟上架"/> 
+        <AlbumWrapper>
+            <SJQThemeHeaderRcm title="新碟上架" /> 
+            <div className="content">
+                <button className="arrow arrow-left sprite_02"
+                        onClick={e=>pageRef.current.prev()}></button>
+                <div className="album">
+                    <Carousel dots={false} ref={pageRef}>
+                        {
+                            [0,1].map(item =>{
+                                return (
+                                    <div key={item} className="page">
+                                        {
+                                            newAlbums.slice(item*5,(item+1)*5).map(iten =>{
+                                                return <SJQAlbumCover key={iten.id}
+                                                                      info={iten}
+                                                                      size={100}
+                                                                      width={118}
+                                                                      bgp="-570px"  
+                                                />
+                                            })
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                    </Carousel>
+                </div>
+                <button className="arrow arrow-right sprite_02"
+                        onClick={e=>pageRef.current.next()}></button>
+            </div>
+        </AlbumWrapper>
+
     )
 })
